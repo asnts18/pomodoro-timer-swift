626fb55b5eb8bcb53ce42f3afe84c0bc7665a90d
Abegail Santos <abegailsantos.1819@gmail.com>
2024-12-10 22:11:54 -0500

    Initial Commit

---
+++ Pomodoro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by Abegail Santos on 12/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PomodoroTests/PomodoroTests.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTests.swift
+//  PomodoroTests
+//
+//  Created by Abegail Santos on 12/10/24.
+//
+
+import Testing
+@testable import Pomodoro
+
+struct PomodoroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
